1d8c4751732133ab796c79b21f11ea200a98d235
Lasse Todt <lassetodt@mailbox.org>
2020-05-12 22:30:43 +0200

    init

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace camera_capture_dotnetcore.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
